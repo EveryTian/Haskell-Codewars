78b08b1327bc63d14ddb4bfff493d15405058bd9
RenHaotian <haotian_ren@outlook.com>
2018-02-09 16:32:29 +0800

    Could not `import Text.Regex.Posix`.

---
+++ 6kyu/valid-phone-number.hs
@@ -0,0 +1,14 @@
+-- https://www.codewars.com/kata/valid-phone-number
+
+module Codewars.Kata.Phone where
+
+-- import Text.Regex.Posix
+
+-- validPhoneNumber :: String -> Bool
+-- validPhoneNumber = ( =~ "\\([0-9]{3}\\) [0-9]{3}-[0-9]{4}")
+
+validPhoneNumber :: String -> Bool
+validPhoneNumber = (==) "(ddd) ddd-dddd" . f
+    where f "" = ""
+          f (x:xs) = (if x `elem` "0123456789" then 'd'
+                      else if x == 'd' then '\'' else x):(f xs)
