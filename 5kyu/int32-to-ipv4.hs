f46ed6999528ef29c4cdacb19ec450a0c8ce4129
RenHaotian <haotian_ren@outlook.com>
2018-02-11 18:52:25 +0800

    Type signature is wrong.

---
+++ 5kyu/int32-to-ipv4.hs
@@ -0,0 +1,12 @@
+-- https://www.codewars.com/kata/int32-to-ipv4
+
+module IPv4 where
+import Data.Int (Int32)
+import Data.Word (Word32)
+import Data.Bits (shiftR, (.&.))
+
+type IPString = String
+
+int32ToIP :: Int32 -> IPString
+int32ToIP int32 = foldl1 (\ x y -> x ++ "." ++ y) $ map (\ x -> show (shiftR word32 x .&. 0xFF)) [24, 16, 8, 0]
+    where word32 = (fromIntegral int32) :: Word32
