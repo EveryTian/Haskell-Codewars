69c08f64e31b45f4dba9d79c9d7193d494ac3b02
RenHaotian <haotian_ren@outlook.com>
2018-04-14 18:21:39 +0800

    List Ops

---
+++ exercism/list-ops/src/ListOps.hs
@@ -0,0 +1,48 @@
+module ListOps
+  ( length
+  , reverse
+  , map
+  , filter
+  , foldr
+  , foldl'
+  , (++)
+  , concat
+  ) where
+
+import Prelude hiding
+  ( length, reverse, map, filter, foldr, (++), concat )
+
+foldl' :: (b -> a -> b) -> b -> [a] -> b
+-- foldl' _ z [] = z
+-- foldl' f z (x:xs) = foldl' f (f z x) xs
+foldl' f z0 xs = foldr f' id xs z0
+    where f' x k z = k $! f z x
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr _ z [] = z
+foldr f z (x:xs) = f x (foldr f z xs)
+
+length :: [a] -> Int
+length = foldl' (\ c _ -> c + 1) 0
+
+reverse :: [a] -> [a]
+reverse = f []
+    where f xs [] = xs
+          f xs (y:ys) = f (y:xs) ys
+
+map :: (a -> b) -> [a] -> [b]
+map _ [] = []
+map f (x:xs) = f x : map f xs
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter _ [] = []
+filter p (x:xs)
+    | p x = x : filter p xs
+    | otherwise = filter p xs
+
+(++) :: [a] -> [a] -> [a]
+[] ++ ys = ys
+(x:xs) ++ ys = x : (xs ++ ys)
+
+concat :: [[a]] -> [a]
+concat = foldr (++) []
