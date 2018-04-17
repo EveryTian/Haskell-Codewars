e55ed9da5af3b3c6a8dc7c8f7ed3a078ee78e6d1
RenHaotian <haotian_ren@outlook.com>
2018-04-17 15:56:59 +0800

    Refactor code with `pure`

--- exercism/anagram/src/Anagram.hs
+++ exercism/anagram/src/Anagram.hs
@@ -4,9 +4,7 @@ import Data.List (sort)
 import Data.Char (toLower)
 
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs xss =
-    let s1 = map toLower xs
-        s2 = sort s1
-    in [i | i <- xss, let x1 = map toLower i
-                          x2 = sort x1
-                      in s2 == x2 && s1 /= x1]
+anagramsFor xs xss = [i | i <- xss, 
+    x <- pure $ map toLower i,
+    s <- pure $ map toLower xs, 
+    sort s == sort x && s /= x]
