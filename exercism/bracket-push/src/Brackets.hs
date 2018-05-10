55e7169674c037dbb61a0c501b65605934cdade7
RenHaotian <haotian_ren@outlook.com>
2018-05-11 00:30:04 +0800

    Refactor with pattern matching

--- exercism/bracket-push/src/Brackets.hs
+++ exercism/bracket-push/src/Brackets.hs
@@ -3,9 +3,10 @@ module Brackets (arePaired) where
 arePaired :: String -> Bool
 arePaired xs = f xs ""
     where f "" stack = null stack
-          f (']':xs) stack = not (null stack) && head stack == '[' && f xs (tail stack)
-          f ('}':xs) stack = not (null stack) && head stack == '{' && f xs (tail stack)
-          f (')':xs) stack = not (null stack) && head stack == '(' && f xs (tail stack)
+          f (']':xs) ('[':ys) = f xs ys
+          f ('}':xs) ('{':ys) = f xs ys
+          f (')':xs) ('(':ys) = f xs ys
           f (x:xs) stack
+              | x `elem` "])}" = False
               | x `elem` "[({" = f xs (x:stack)
               | otherwise = f xs stack
