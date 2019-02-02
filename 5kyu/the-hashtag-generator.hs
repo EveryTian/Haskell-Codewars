c7494f80cb492c60c92653ad339be85ca079e5a5
EveryTian <haotian_ren@outlook.com>
2019-02-02 13:50:30 +0800

    Rewrite in Weekly Coding Challenges (2018/8/28)

--- 5kyu/the-hashtag-generator.hs
+++ 5kyu/the-hashtag-generator.hs
@@ -5,8 +5,11 @@ module Codewars.Kata.Hashtag where
 import Data.Char (toUpper)
 
 generateHashtag :: String -> Maybe String
-generateHashtag str
-    | length str > 140 || words str == [] = Nothing
-    | otherwise = Just . ('#' :) . foldl1 (++) . map upperFstChar . words $ str
-    where upperFstChar "" = ""
-          upperFstChar (x:xs) = (toUpper x):xs
+-- generateHashtag str
+--    | length str > 140 || words str == [] = Nothing
+--    | otherwise = Just . ('#' :) . foldl1 (++) . map upperFstChar . words $ str
+--    where upperFstChar "" = ""
+--          upperFstChar (x:xs) = (toUpper x):xs
+generateHashtag s =
+  let ss = '#':(concat $ map (\ (x:xs) -> (toUpper x):xs) $ words s)
+  in if length ss > 140 || length ss == 1 then Nothing else Just ss
