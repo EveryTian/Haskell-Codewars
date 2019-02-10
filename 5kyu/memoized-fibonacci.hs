d5f1b40fb21130b5e8b3902175e409d094164d2a
EveryTian <haotian_ren@outlook.com>
2019-02-10 16:48:40 +0800

    Rewrite and correct How could it pass...

--- 5kyu/memoized-fibonacci.hs
+++ 5kyu/memoized-fibonacci.hs
@@ -3,8 +3,12 @@
 module Fibonacci where
 
 fibonacci :: Int -> Integer
-fibonacci n = (fibList n) !! (n)
-    where fibList 0 = [0]
-          fibList 1 = [0, 1]
-          fibList n = listN ++ [(listN !! (n - 1)) + (listN !! (n - 2))]
-              where listN = fibList (n - 1)
+-- fibonacci n = fibList n !! n
+--     where fibList 0 = [0]
+--           fibList 1 = [0, 1]
+--           fibList n = listN ++ [(listN !! (n - 1)) + (listN !! (n - 2))]
+--               where listN = fibList (n - 1)
+fibonacci n = fibs !! n
+
+fibs :: [Integer]
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
