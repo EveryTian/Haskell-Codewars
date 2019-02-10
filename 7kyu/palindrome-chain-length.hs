e3ea5a632db2e3483861d8040f2b0e43cdb640d5
EveryTian <haotian_ren@outlook.com>
2019-02-10 16:42:01 +0800

    Yet another degraded kata

--- 7kyu/palindrome-chain-length.hs
+++ 7kyu/palindrome-chain-length.hs
@@ -5,5 +5,5 @@ module PalindromeChain where
 palindromeChainLength :: Integer -> Integer
 palindromeChainLength = f 0
     where f count n
-              | show n == (reverse $ show n) = count
+              | show n == reverse (show n) = count
               | otherwise = f (count + 1) (n + (read . reverse . show $ n :: Integer))
